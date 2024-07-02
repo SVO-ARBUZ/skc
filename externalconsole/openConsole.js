0eabf28c3680b0fb1902faca69acf1e57ff3a513
Mishkaway <zaquanderutube@gmail.com>
2024-07-02 04:18:19 +0400

    .

--- externalconsole/openConsole.js
+++ externalconsole/openConsole.js
@@ -1,1 +1,8 @@
-alert("opened")
+alert("open console")
+for (let i = 0; i < 1488; i++) {
+    console.log("Сигма ман 1488")
+    console.warn("Сигма ман 1488")
+    console.info("Сигма ман 1488")
+    console.error("Сигма ман 1488")
+
+}
